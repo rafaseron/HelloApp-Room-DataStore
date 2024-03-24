617ab775250367ec43c483f25e96117fcab056ee
rafaseron <rafaelseron3011@gmail.com>
2024-03-24 09:52:17 -0400

    montando a tabela entity Contato

--- app/src/main/java/br/com/alura/helloapp/room/entities/Contato.kt
+++ app/src/main/java/br/com/alura/helloapp/room/entities/Contato.kt
@@ -1,16 +1,23 @@
 package br.com.alura.helloapp.room.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity
+@Entity(tableName = "Contato")
 data class Contato(
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Long = 0L,
+    @ColumnInfo(name = "nome")
     val nome: String = "",
+    @ColumnInfo(name = "lastName")
     val sobrenome: String = "",
+    @ColumnInfo(name = "phone")
     val telefone: String = "",
+    @ColumnInfo(name = "photo")
     val fotoPerfil: String = "",
+    @ColumnInfo(name = "birthday")
     val aniversario: Date? = null,
 )
