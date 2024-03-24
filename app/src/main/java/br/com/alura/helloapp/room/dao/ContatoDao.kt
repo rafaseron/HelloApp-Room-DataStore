c0eeb26d9308920ab248bff73a46d806102b94b4
rafaseron <rafaelseron3011@gmail.com>
2024-03-24 13:42:57 -0400

    CRUD no Banco de Dados + Algumas Consultas personalizadas

--- app/src/main/java/br/com/alura/helloapp/room/dao/ContatoDao.kt
+++ app/src/main/java/br/com/alura/helloapp/room/dao/ContatoDao.kt
@@ -1,16 +1,34 @@
 package br.com.alura.helloapp.room.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import br.com.alura.helloapp.room.entities.Contato
+import java.util.Date
 
 @Dao
 interface ContatoDao {
 
     @Insert
-    fun insere(contato: Contato)
+    fun insert(contato: Contato): Long  //podemos retornar em Long quantos dados foram inseridos
 
-    @Query("SELECT * FROM Contato")
-    fun buscaTodos(): List<Contato>
+    @Update
+    fun update(contato: Contato): Int //podemos retornar em Int quantos dados foram atualizados
+
+    @Delete
+    fun delete(contato: Contato)
+
+    //Query são CONSULTAS no Banco de Dados usando SQL
+    @Query("SELECT * FROM Contato") //tambem podemos adicionar WHERE e AND se necessario
+    fun getAll(): List<Contato>
+    //WHERE ajudaria a pesquisar por um id especifico por exemplo. AND ajudaria a adicionar mais logica a consulta, se preciso
+
+    @Query("SELECT * FROM Contato WHERE birthday = :dataDeHoje")
+    fun getAniversariantes(dataDeHoje: Date): List<Contato>
+
+    @Query("SELECT * FROM Contato WHERE id = :idToSearch")
+    fun getContactFromId(idToSearch: Long): Contato
+
 }
