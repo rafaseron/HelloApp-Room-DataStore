ef2ba64264efa92887f4641e769faea55db79993
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 19:20:10 -0400

    add typeConverter Date/Long

---
+++ app/src/main/java/br/com/alura/helloapp/room/typeConverters/TypeConverter.kt
@@ -0,0 +1,25 @@
+package br.com.alura.helloapp.room.typeConverters
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+
+class TypeConverter {
+
+    @TypeConverter
+    fun convertDateToLong(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun convertLongToDate(longDate: Long?): Date?{
+
+        return longDate?.let {
+            long ->
+            Date(long)
+        }
+
+    }
+
+
+}
