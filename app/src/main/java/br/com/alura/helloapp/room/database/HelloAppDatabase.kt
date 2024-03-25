40e3cc9d9fd23ed162e885389b8eefa5eda3994e
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 19:20:32 -0400

    habilitando typeConverter no DB

--- app/src/main/java/br/com/alura/helloapp/room/database/HelloAppDatabase.kt
+++ app/src/main/java/br/com/alura/helloapp/room/database/HelloAppDatabase.kt
@@ -4,19 +4,21 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import br.com.alura.helloapp.room.typeConverters.TypeConverter
 import br.com.alura.helloapp.room.dao.ContatoDao
 import br.com.alura.helloapp.room.entities.Contato
-import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [Contato::class], version = 1)
+@TypeConverters(TypeConverter::class)
 abstract class HelloAppDatabase : RoomDatabase() {
     abstract fun contatoDao(): ContatoDao
 
     // Uma @Database precisa ser uma classe absrata em Kotlin
     // Para criar instancias de algo abstrato, voce tem que colocar o que voce quer instanciar dentro do
     // escopo de companion object { }. Dessa forma, por exemplo, getDataBase fica acessivel
-    companion object {
 
+    companion object {
         private lateinit var INSTANCE: HelloAppDatabase
         fun getDatabase(context: Context): HelloAppDatabase {
             // verifica se o banco de dados esta inicializado. Se estiver, retorna. Se não, inicializa e retorna (Singleton)
@@ -32,4 +34,6 @@ abstract class HelloAppDatabase : RoomDatabase() {
             }
         }
     }
+
+
 }
