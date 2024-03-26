eeabe7aecd96a0ad5d952d59b6999f1703364f23
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 22:25:42 -0400

    Fornecendo a funcao de Provide para Mandar a Injecao do ContatoRepository com o Contexto inserido

--- app/src/main/java/br/com/alura/helloapp/dependecyInjection/DatabaseModule.kt
+++ app/src/main/java/br/com/alura/helloapp/dependecyInjection/DatabaseModule.kt
@@ -1,2 +1,20 @@
 package br.com.alura.helloapp.dependecyInjection
 
+import android.content.Context
+import br.com.alura.helloapp.room.repository.ContatoRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule{
+    @Provides
+    fun provideContatoRepository(@ApplicationContext context: Context): ContatoRepository{
+        return ContatoRepository(context = context)
+    }
+
+}
