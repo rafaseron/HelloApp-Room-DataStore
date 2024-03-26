e1be29523fb326d38d1e59ad61462eed6b7abde3
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 21:33:19 -0400

    add fun to update uiState List

--- app/src/main/java/br/com/alura/helloapp/ui/viewmodels/ListaContatosViewModel.kt
+++ app/src/main/java/br/com/alura/helloapp/ui/viewmodels/ListaContatosViewModel.kt
@@ -1,6 +1,7 @@
 package br.com.alura.helloapp.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import br.com.alura.helloapp.room.entities.Contato
 import br.com.alura.helloapp.ui.uiState.ListaContatosUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,5 +13,9 @@ class ListaContatosViewModel(
     private val _uiState = MutableStateFlow(ListaContatosUiState())
     val uiState: StateFlow<ListaContatosUiState>
         get() = _uiState.asStateFlow()
+
+    fun getContactList(list: List<Contato>){
+        _uiState.value = _uiState.value.copy(contatos = list)
+    }
 
 }
