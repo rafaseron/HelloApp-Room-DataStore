15402cf628486b3f42c92b6c201706ee3ae9c0ae
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 22:23:56 -0400

    Recebendo ContatoRepository por Injecao de Dependencia para nao precisar se preocupar com o Contexto

--- app/src/main/java/br/com/alura/helloapp/ui/viewmodels/ListaContatosViewModel.kt
+++ app/src/main/java/br/com/alura/helloapp/ui/viewmodels/ListaContatosViewModel.kt
@@ -1,21 +1,32 @@
 package br.com.alura.helloapp.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.helloapp.room.entities.Contato
+import br.com.alura.helloapp.room.repository.ContatoRepository
 import br.com.alura.helloapp.ui.uiState.ListaContatosUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ListaContatosViewModel(
-) : ViewModel() {
+@HiltViewModel
+class ListaContatosViewModel @Inject constructor(private val contatoRepository: ContatoRepository) : ViewModel() {
 
     private val _uiState = MutableStateFlow(ListaContatosUiState())
     val uiState: StateFlow<ListaContatosUiState>
         get() = _uiState.asStateFlow()
 
-    fun getContactList(list: List<Contato>){
+    private fun updateContactList(list: List<Contato>){
         _uiState.value = _uiState.value.copy(contatos = list)
+    }
+
+    init {
+        viewModelScope.launch {
+            updateContactList(contatoRepository.getAllContacts())
+        }
     }
 
 }
