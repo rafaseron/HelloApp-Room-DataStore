2176af9d6016f8adf30c249b170ec3697e0675fa
rafaseron <rafaelseron3011@gmail.com>
2024-03-25 21:32:44 -0400

    inserir o Contato no DB no Navigation

--- app/src/main/java/br/com/alura/helloapp/navigation/FormularioContatoNavigation.kt
+++ app/src/main/java/br/com/alura/helloapp/navigation/FormularioContatoNavigation.kt
@@ -3,15 +3,19 @@ package br.com.alura.helloapp.navigation
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import br.com.alura.helloapp.R
+import br.com.alura.helloapp.room.entities.Contato
+import br.com.alura.helloapp.room.repository.ContatoRepository
 import br.com.alura.helloapp.ui.screens.FormularioContatoTela
 import br.com.alura.helloapp.ui.viewmodels.FormularioContatoViewModel
 import br.com.alura.helloapp.util.ID_CONTATO
+import kotlinx.coroutines.launch
 
 fun NavGraphBuilder.formularioContatoGraph(
     navController: NavHostController
@@ -27,6 +31,7 @@ fun NavGraphBuilder.formularioContatoGraph(
             val viewModel = hiltViewModel<FormularioContatoViewModel>()
             val state by viewModel.uiState.collectAsState()
             val context = LocalContext.current
+            val contatoRepository = ContatoRepository(context)
 
             LaunchedEffect(state.aniversario) {
                 viewModel.defineTextoAniversario(
@@ -34,9 +39,14 @@ fun NavGraphBuilder.formularioContatoGraph(
                 )
             }
 
+            val scope = rememberCoroutineScope()
+
             FormularioContatoTela(
                 state = state,
                 onClickSalvar = {
+                    scope.launch {
+                        contatoRepository.insertOnDatabase(Contato(nome = state.nome, sobrenome = state.sobrenome, telefone = state.telefone, fotoPerfil = state.fotoPerfil, aniversario = state.aniversario))
+                    }
                     navController.popBackStack()
                 },
                 onCarregarImagem = {
