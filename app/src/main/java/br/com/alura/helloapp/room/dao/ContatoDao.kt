383a0b5e81b30068117b7101b88b688aed8a8477
rafaseron <rafaelseron3011@gmail.com>
2024-03-26 23:59:08 -0400

    se retorna Flow nao pode ser suspend fun

--- app/src/main/java/br/com/alura/helloapp/room/dao/ContatoDao.kt
+++ app/src/main/java/br/com/alura/helloapp/room/dao/ContatoDao.kt
@@ -24,13 +24,13 @@ interface ContatoDao {
 
     //Query são CONSULTAS no Banco de Dados usando SQL
     @Query("SELECT * FROM Contato") //tambem podemos adicionar WHERE e AND se necessario
-    suspend fun getAll(): Flow<List<Contato>>
+    fun getAll(): Flow<List<Contato>>
     //WHERE ajudaria a pesquisar por um id especifico por exemplo. AND ajudaria a adicionar mais logica a consulta, se preciso
 
     @Query("SELECT * FROM Contato WHERE birthday = :dataDeHoje")
     suspend fun getAniversariantes(dataDeHoje: Date): List<Contato>
 
     @Query("SELECT * FROM Contato WHERE id = :idToSearch")
-    suspend fun getContactFromId(idToSearch: Long): Flow<Contato?>
+    fun getContactFromId(idToSearch: Long): Flow<Contato?>
 
 }
