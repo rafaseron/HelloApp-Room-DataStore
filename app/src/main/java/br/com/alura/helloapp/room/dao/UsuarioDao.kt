968d2104947e468814c65859accbb020f655d553
rafaseron <rafaelseron3011@gmail.com>
2024-03-27 23:37:08 -0400

    add new Dao -> UsuarioDao

---
+++ app/src/main/java/br/com/alura/helloapp/room/dao/UsuarioDao.kt
@@ -0,0 +1,29 @@
+package br.com.alura.helloapp.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import br.com.alura.helloapp.room.entities.Usuario
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UsuarioDao {
+
+    @Insert
+    suspend fun insertUser(usuario: Usuario)
+
+    @Delete
+    suspend fun deleteUser(usuario: Usuario)
+
+    @Update
+    suspend fun updateUser(usuario: Usuario)
+
+    @Query("SELECT * FROM Usuario")
+    fun getAllUsers(): Flow<List<Usuario>>
+
+    @Query("SELECT * FROM Usuario WHERE usuario = :username")
+    suspend fun searchUserFromUsername(username: String): Usuario?
+
+}
