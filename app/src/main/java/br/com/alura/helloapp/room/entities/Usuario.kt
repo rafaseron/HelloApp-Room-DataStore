5aaed48a920aac1d2b8ddc679c9ea277bbb5771b
rafaseron <rafaelseron3011@gmail.com>
2024-03-27 23:36:48 -0400

    add new Entity -> Usuario

---
+++ app/src/main/java/br/com/alura/helloapp/room/entities/Usuario.kt
@@ -0,0 +1,14 @@
+package br.com.alura.helloapp.room.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Usuario")
+data class Usuario (@ColumnInfo(name = "nome")
+                    val nome: String = "",
+                    @PrimaryKey
+                    @ColumnInfo(name = "usuario")
+                    val usuario: String = "",
+                    @ColumnInfo(name = "senha")
+                    val senha: String = "")
